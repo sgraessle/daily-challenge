5ee1809b17f3208b5ea3dc32c9ef923debf6e6ef
Scott Graessle <scott@teamchaos.com>
2015-11-18 21:29:15 -0600

    Add HMAC.

---
+++ Daily Challenge/TC-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  TC-Bridging-Header.h
+//  Dummy Game
+//
+//  Created by Scott  Graessle on 11/16/15.
+//  Copyright © 2015 Team Chaos. All rights reserved.
+//
+
+#ifndef TC_Bridging_Header_h
+#define TC_Bridging_Header_h
+
+#import <CommonCrypto/CommonHMAC.h>
+
+#endif /* TC_Bridging_Header_h */
